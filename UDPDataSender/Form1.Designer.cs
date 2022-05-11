98c415792942eb31cadf5c7865f85f7978ea593d
Mustafa Kızılışık <mkizilisik@outlook.com>
2022-05-11 12:30:42 +0300

    Add project files.

---
+++ UDPDataSender/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace UDPDataSender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUDPPort = new System.Windows.Forms.TextBox();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(224, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UDP Port:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(224, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Data:";
+            // 
+            // txtUDPPort
+            // 
+            this.txtUDPPort.Location = new System.Drawing.Point(292, 192);
+            this.txtUDPPort.Name = "txtUDPPort";
+            this.txtUDPPort.Size = new System.Drawing.Size(92, 20);
+            this.txtUDPPort.TabIndex = 2;
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(292, 158);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(207, 20);
+            this.txtData.TabIndex = 3;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(424, 191);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.txtUDPPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUDPPort;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ UDPDataSender/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace UDPDataSender
+{
+    public partial class Form1 : Form
+    {
+        List<string> deviceDataList = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //for (int i = 0; i < 15; i++)
+            //{
+            //    deviceDataList.Add(DeviceDataGenerate());
+            //}
+
+            DeviceDataGenerate();
+
+            while (true)
+            {
+                Send();
+            }
+        }
+
+        private List<string> DeviceDataGenerate()
+        {
+            //Random rnd = new Random();
+            //char[] macAddressList = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+            //string macAddress = "";
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    macAddress += macAddressList[rnd.Next(15)].ToString();
+            //}
+            //string deviceNo = rnd.Next(99).ToString();
+            //return $"%CMS{macAddress}{deviceNo.PadLeft(3, '0')}111111111111111111111111000000001111111100000000000**..5";
+
+            string[] macAddressList = { "1111", "2222", "3333", "4444", "5555", "6666", "7777", "8888", "9999", "1010", "1111", "1212", "1313", "1414", "1515", "1616" };
+
+            string[] deviceNoList = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16" };
+
+            for (int i = 0; i < 16; i++)
+            {
+                var data = $"%CMS{macAddressList[i]}{deviceNoList[i].PadLeft(3, '0')}111111111111111111111111000000001111111100000000000**..5";
+                deviceDataList.Add(data);
+            }
+            return deviceDataList;
+        }
+        private void Send()
+        {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            IPAddress serverAddr = IPAddress.Parse("192.168.2.255");
+
+            IPEndPoint endPoint = new IPEndPoint(serverAddr, Convert.ToInt32(txtUDPPort.Text));
+
+            //string data = txtData.Text;
+
+            foreach (var data in deviceDataList)
+            {
+                byte[] send_buffer = Encoding.ASCII.GetBytes(data);
+
+                sock.SendTo(send_buffer, endPoint);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
